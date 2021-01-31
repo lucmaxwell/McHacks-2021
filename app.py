0d95f8bea859ccd349de5c1562b3f0d139747833
lucmaxwell <lucmaxwell8@gmail.com>
2021-01-31 02:32:09 -0600

    Added identifiers

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def hello_world():
     output = ""
     for i in range(len(matched_titles)):
         currContent = matched_content[i][0][0].split("\n")[0] + "..."
-        output += matched_titles[i][0][0] + "\n" + currContent + "\n" + matched_url[i][0][0] + "\n\n\n\n"
+        output += matched_titles[i][0][0] + "\n\n\n" + currContent + "\n\n\n" + matched_url[i][0][0] + "\n\n\n\n"
 
     return jsonify({'message' : output})
 
